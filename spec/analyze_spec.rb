5a4e83fe515d32112700e1b78f89f9bc49c9fc97
Andrew Broman <broman@acm.org>
2012-03-19 15:04:24 -0500

    Readjust spec

--- spec/analyze_spec.rb
+++ spec/analyze_spec.rb
@@ -6,33 +6,33 @@ describe Analyzer do
     @analyzer = Analyzer.new
   end
 
-  describe Analyzer do
-    describe "new analyzer" do
-      it "returns a new analyzer object" do
-        @analyzer.should be_an_instance_of Analyzer
-      end
-
-      it "accepts zero parameters" do
-        analyzer = Analyzer.new
-        analyzer.should be_an_instance_of Analyzer
-      end
-
-      it "accepts a file as a parameter and sets the @filename variable" do
-        analyzer = Analyzer.new("data/samplefile.html")
-        analyzer.should be_an_instance_of Analyzer
-        analyzer.filename.should == "data/samplefile.html"
-      end
-
-      it "accepts a directory as a parameter and set the @directory variable" do
-        analyzer = Analyzer.new("data/")
-        analyzer.should be_an_instance_of Analyzer
-        analyzer.directory.should == "data/"
-      end
-
-      it "raise exception for non-existent item" do
-        expect { Analyzer.new("nonexist") }.to raise_error(RuntimeError) 
-      end
+  describe "new analyzer" do
+    it "returns a new analyzer object" do
+      @analyzer.should be_an_instance_of Analyzer
     end
+
+    it "accepts zero parameters" do
+      analyzer = Analyzer.new
+      analyzer.should be_an_instance_of Analyzer
+    end
+
+    it "accepts a file as a parameter and sets the @filename variable" do
+      analyzer = Analyzer.new("data/samplefile.html")
+      analyzer.should be_an_instance_of Analyzer
+      analyzer.filename.should == "data/samplefile.html"
+    end
+
+    it "accepts a directory as a parameter and set the @directory variable" do
+      analyzer = Analyzer.new("data/")
+      analyzer.should be_an_instance_of Analyzer
+      analyzer.directory.should == "data/"
+    end
+
+    it "raise exception for non-existent item" do
+      expect { Analyzer.new("nonexist") }.to raise_error(RuntimeError) 
+    end
+
+    it "ensures filename has a specific format"
   end
 
 end
