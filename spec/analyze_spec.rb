d9b65c666181e8bb19689a12b13c2f59bd6ae74a
Andrew Broman <broman@acm.org>
2012-03-19 14:48:51 -0500

    Adjust structure of rspec descriptions

--- spec/analyze_spec.rb
+++ spec/analyze_spec.rb
@@ -1,36 +1,38 @@
 require 'spec_helper'
 
 describe Analyzer do
-  
+
   before :each do
     @analyzer = Analyzer.new
   end
 
   describe Analyzer do
-    it "returns a new analyzer object" do
-      @analyzer.should be_an_instance_of Analyzer
-    end
+    describe "new analyzer" do
+      it "returns a new analyzer object" do
+        @analyzer.should be_an_instance_of Analyzer
+      end
 
-    it "accepts zero parameters" do
-      analyzer = Analyzer.new
-      analyzer.should be_an_instance_of Analyzer
-    end
+      it "accepts zero parameters" do
+        analyzer = Analyzer.new
+        analyzer.should be_an_instance_of Analyzer
+      end
 
-    it "accepts a file as a parameter and sets the @filename variable" do
-      analyzer = Analyzer.new("data/samplefile.html")
-      analyzer.should be_an_instance_of Analyzer
-      analyzer.filename.should == "data/samplefile.html"
-    end
+      it "accepts a file as a parameter and sets the @filename variable" do
+        analyzer = Analyzer.new("data/samplefile.html")
+        analyzer.should be_an_instance_of Analyzer
+        analyzer.filename.should == "data/samplefile.html"
+      end
 
-    it "accepts a directory as a parameter and set the @directory variable" do
-      analyzer = Analyzer.new("data/")
-      analyzer.should be_an_instance_of Analyzer
-      analyzer.directory.should == "data/"
-    end
+      it "accepts a directory as a parameter and set the @directory variable" do
+        analyzer = Analyzer.new("data/")
+        analyzer.should be_an_instance_of Analyzer
+        analyzer.directory.should == "data/"
+      end
 
-    it "raise exception for non-existent item" do
-      expect { Analyzer.new("nonexist") }.to raise_error(RuntimeError) 
+      it "raise exception for non-existent item" do
+        expect { Analyzer.new("nonexist") }.to raise_error(RuntimeError) 
+      end
     end
   end
-  
+
 end
